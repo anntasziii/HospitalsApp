b462376943e4145ad57fedd7b64c2865a3c34c5f
anntasziii <anntesziii.kushnir@gmail.com>
2025-03-27 15:19:30 +0100

    admin panel

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
-// use App\Models\Brand;
-// use App\Models\Category;
-// use App\Models\Order;
-// use App\Models\Product;
-// use App\Models\User;
+
+use App\Models\User;
+use App\Models\Doctor;
+use App\Models\Analysis;
+use App\Models\Hospital;
+use App\Models\Type;
+use App\Models\Order;
+use App\Models\Slider;
+
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -14,22 +18,25 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // $todayDate = Carbon::now()->format('d-m-Y');
-        // $thisMonth = Carbon::now()->format('m');
-        // $thisYear = Carbon::now()->format('Y');
-        // // $totalProducts = Product::count();
-        // $totalCategories = Category::count();
-        // $totalBrands = Brand::count();
-        // $totalAllUsers = User::count();
-        // $totalUser = User::where('role_as', '0')->count();
-        // $totalAdmin = User::where('role_as', '1')->count();
-        // $totalOrder = Order::count();
-        // $todayOrder = Order::whereDate('created_at', $todayDate)->count();
-        // $totalMonthOrder = Order::whereMonth('created_at', $thisMonth)->count();
-        // $totalYearOrder = Order::whereYear('created_at', $thisYear)->count();
-        // return view('admin.dashboard', compact('totalProducts', 'totalCategories', 'totalBrands',
-        //                                         'totalAllUsers', 'totalUser', 'totalAdmin', 'totalOrder',
-        //                                         'todayOrder', 'totalMonthOrder', 'totalYearOrder'));
-        return view('admin.dashboard');
+        $todayDate = Carbon::now()->format('d-m-Y');
+        $thisMonth = Carbon::now()->format('m');
+        $thisYear = Carbon::now()->format('Y');
+
+        $totalDoctors = Doctor::count();
+        $totalAnalyses = Analysis::count();
+        $totalHospitals = Hospital::count();
+        $totalTypes = Type::count();
+        $totalOrder = Order::count();
+        $totalUsers = User::where('role_as', '0')->count();
+        $totalAdmin = User::where('role_as', '1')->count();
+        $totalAllUsers = User::count();
+        $todayOrder = Order::whereDate('created_at', $todayDate)->count();
+        $totalMonthOrder = Order::whereMonth('created_at', $thisMonth)->count();
+        $totalYearOrder = Order::whereYear('created_at', $thisYear)->count();
+        $totalSliders = Slider::count();
+
+        return view('admin.dashboard', compact('totalDoctors', 'totalAnalyses', 'totalHospitals', 'totalTypes',
+                                                'totalAllUsers', 'totalUsers', 'totalAdmin', 'totalOrder',
+                                                'todayOrder', 'totalMonthOrder', 'totalYearOrder', 'totalSliders'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,100 +7,110 @@
             <h6 class="alert alert-success">{{ session('message') }}</h6>
         @endif
         <div class="me-md-3 me-xl-5">
-            <h2>ADMIN PANEL</h2>
+            <h1 style="color: #002266; letter-spacing: 1px;">ADMIN PANEL:</h1>
             <p class="mb-md-0" style="font-size: 16px">Hospital system analytics for administrators</p>
             <hr>
         </div>
         <div class="row">
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-1">
-                    <label class="fw-bold">Total Orders</label>
-                    {{-- <h1>{{$totalOrder}}</h1> --}}
-                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card card-body text-white mb-3 card-admin card-admin-2">
-                    <label class="fw-bold">Today Orders</label>
-                    {{-- <h1>{{$todayOrder}}</h1> --}}
-                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card card-body text-white mb-3 card-admin card-admin-3">
-                    <label class="fw-bold">This Month Orders</label>
-                    {{-- <h1>{{$totalMonthOrder}}</h1> --}}
-                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card card-body text-white mb-3 card-admin card-admin-4">
-                    <label class="fw-bold">Year Orders</label>
-                    {{-- <h1>{{$totalYearOrder}}</h1> --}}
-                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-3">
-                <div class="card card-body text-white mb-3 card-admin card-admin-1">
-                    <label class="fw-bold">Total Products</label>
-                    {{-- <h1>{{$totalProducts}}</h1> --}}
-                    <a href="{{url('admin/products')}}" class="text-white text-view">View</a>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card card-body text-white mb-3 card-admin card-admin-2">
-                    <label class="fw-bold">
-                        Total Hospitals
+                    <label class="fw-bold">Total Referrals:
+                        <span style="color: white; font-size: 26px;">{{$totalOrder}}</span>
                     </label>
-                    {{-- <h1>{{$totalCategories}}</h1> --}}
-                    <a href="{{url('admin/hospital')}}" class="text-white text-view">View</a>
-
-
+                    <div class="d-flex justify-content-between align-items-center">
+                        <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body text-white mb-3 card-admin card-admin-2">
+                    <label class="fw-bold">Today Referrals:
+                        <span style="color: white; font-size: 26px;">{{$todayOrder}}</span>
+                    </label>
+                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-3">
-                    <label class="fw-bold">Total Brands</label>
-                    {{-- <h1>{{$totalBrands}}</h1> --}}
-                    <a href="{{url('admin/brands')}}" class="text-white text-view">View</a>
+                    <label class="fw-bold">This Month Referrals:
+                        <span style="color: white; font-size: 26px;">{{$totalMonthOrder}}</span>
+                    </label>
+                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-4">
-                    <label class="fw-bold">Total Times</label>
-                    {{-- <h1>{{$totalTimes}}</h1> --}}
-                    <a href="{{url('admin/times')}}" class="text-white text-view">View</a>
+                    <label class="fw-bold">This Year Referrals:
+                        <span style="color: white; font-size: 26px;">{{$totalYearOrder}}</span>
+                    </label>
+                    <a href="{{url('admin/orders')}}" class="text-white text-view">View</a>
                 </div>
             </div>
         </div>
         <div class="row">
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-1">
-                    <label class="fw-bold">All Users</label>
-                    {{-- <h1>{{$totalAllUsers}}</h1> --}}
+                    <label class="fw-bold">Total Doctors:
+                        <span style="color: white; font-size: 26px;">{{$totalDoctors}}</span>
+                    </label>
+                    <a href="{{url('admin/doctors')}}" class="text-white text-view">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body text-white mb-3 card-admin card-admin-2">
+                    <label class="fw-bold"> Total Analyses:
+                        <span style="color: white; font-size: 26px;">{{$totalAnalyses}}</span>
+                    </label>
+                    <a href="{{url('admin/analyses')}}" class="text-white text-view">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body text-white mb-3 card-admin card-admin-3">
+                    <label class="fw-bold">Total Hospitals:
+                        <span style="color: white; font-size: 26px;">{{$totalHospitals}}</span>
+                    </label>
+                    <a href="{{url('admin/hospital')}}" class="text-white text-view">View</a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card card-body text-white mb-3 card-admin card-admin-4">
+                    <label class="fw-bold">Total Types:
+                        <span style="color: white; font-size: 26px;">{{$totalTypes}}</span>
+                    </label>
+                    <a href="{{url('admin/types')}}" class="text-white text-view">View</a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card card-body text-white mb-3 card-admin card-admin-1">
+                    <label class="fw-bold">Total Registered Persons:
+                        <span style="color: white; font-size: 26px;">{{$totalAllUsers}}</span>
+                    </label>
                     <a href="{{url('admin/users')}}" class="text-white text-view">View</a>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-2">
-                    <label class="fw-bold">Total User</label>
-                    {{-- <h1>{{$totalUser}}</h1> --}}
+                    <label class="fw-bold">Total User:
+                        <span style="color: white; font-size: 26px;">{{$totalUsers}}</span>
+                    </label>
                     <a href="{{url('admin/users')}}" class="text-white text-view">View</a>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-3">
-                    <label class="fw-bold">Total Admin</label>
-                    {{-- <h1>{{$totalAdmin}}</h1> --}}
+                    <label class="fw-bold">Total Admin:
+                        <span style="color: white; font-size: 26px;">{{$totalAdmin}}</span>
+                    </label>
                     <a href="{{url('admin/users')}}" class="text-white text-view">View</a>
                 </div>
             </div>
             <div class="col-md-3">
                 <div class="card card-body text-white mb-3 card-admin card-admin-4">
-                    <label class="fw-bold">Total Sliders</label>
-                    {{-- <h1>{{$totalSliders}}</h1> --}}
+                    <label class="fw-bold">Total Sliders:
+                        <span style="color: white; font-size: 26px;">{{$totalSliders}}</span>
+                    </label>
                     <a href="{{url('admin/sliders')}}" class="text-white text-view">View</a>
                 </div>
             </div>
